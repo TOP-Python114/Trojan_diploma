0894b17fe2c71ed0bf1546b9911ff38d8dc9e68b
Gennadiy <renardes@gmail.com>
2023-01-22 12:44:42 +0500

    обзор проекта: исправления, комментарии

--- mysite/site_vacancy/views.py
+++ mysite/site_vacancy/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'site_vacancy/base.html')
 
 
 def page_vacancy(request):
     return render(request, 'site_vacancy/index.html')
+
