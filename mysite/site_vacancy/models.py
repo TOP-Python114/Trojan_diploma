f2473a11ab3bc0acd912ce49c650b50df2ff60ff
HelenTroyan <93206668+HelenTroyan@users.noreply.github.com>
2023-01-22 22:18:24 +0700

    изменение название класса

--- mysite/site_vacancy/models.py
+++ mysite/site_vacancy/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Position(models.Model):
-    position = models.CharField(db_column='Position', max_length=40)
-    salary = models.DecimalField(db_column='Salary', max_digits=8, decimal_places=2)
-    work_schedule = models.CharField(db_column='Schedule', max_length=40)
+class Vacancy(models.Model):
+    position = models.CharField(db_column='Должность', max_length=40)
+    salary = models.DecimalField(db_column='Заработная плата', max_digits=8, decimal_places=2)
+    work_schedule = models.CharField(db_column='График работы', max_length=40)
     responsibilities = models.TextField('Описание')
     requirements = models.TextField('Описание')
     working_conditions = models.TextField('Описание')
