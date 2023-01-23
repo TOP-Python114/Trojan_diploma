ede91bc35b8b2d817e000381e93e32e8dafb3c89
HelenTroyan <93206668+HelenTroyan@users.noreply.github.com>
2023-01-23 23:17:04 +0700

    переименование класса

--- mysite/site_vacancy/models.py
+++ mysite/site_vacancy/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class Vacancy(models.Model):
-    position = models.CharField(db_column='Должность', max_length=40)
-    salary = models.DecimalField(db_column='Заработная плата', max_digits=8, decimal_places=2)
-    work_schedule = models.CharField(db_column='График работы', max_length=40)
+    position = models.CharField(db_column='Position', max_length=40)
+    salary = models.DecimalField(db_column='Salary', max_digits=8, decimal_places=2)
+    work_schedule = models.CharField(db_column='Work_schedule', max_length=40)
     responsibilities = models.TextField('Описание')
     requirements = models.TextField('Описание')
     working_conditions = models.TextField('Описание')
